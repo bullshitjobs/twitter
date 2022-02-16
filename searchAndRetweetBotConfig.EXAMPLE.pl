1ee5534b89fdbc02ccf5ef6c8ddb4cb6392ceea3
www-data <www-data@_HOSTNAME_>
2022-02-16 02:46:00 +0100

    Typo.

--- searchAndRetweetBotConfig.EXAMPLE.pl
+++ searchAndRetweetBotConfig.EXAMPLE.pl
@@ -1,6 +1,6 @@
 {
   'bot123' => { 
-    consumer_key        => 'Y<API Key>',
+    consumer_key        => '<API Key>',
     consumer_secret     => '<API Secret>',
     access_token        => '<Access Token>',
     access_token_secret => '<Access Secret>',
